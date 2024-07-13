eee12383d34eca21f046370bf3e7529ac0a96f52
ahmedyar7 <ahmedyar778@gmail.com>
2024-07-13 16:09:45 +0500

    Adding a successfull entry

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ from datetime import datetime as dt
 
 class CSV:
     CSV_FILE = "finanice_data.csv"
+    COLUMNS = ["date", "amount", "category", "description"]
 
     @classmethod
-    def initialize_csv():
-        pass
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description,
+        }
+
+        with open(cls.CSV_FILE, mode="a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+
+        print("Entry added successfully")
+
+
+CSV.initialize_csv()
+CSV.add_entry("20-07-2024", 125.25, "Income", "Salary")
