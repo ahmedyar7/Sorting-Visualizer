e2525042ba9f8eaa4614ca8285f02cda2619588a
ahmedyar7 <ahmedyar778@gmail.com>
2024-07-13 15:50:00 +0500

    Getting stated with the project

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import csv
+from datetime import datetime as dt
+
+
+class CSV:
+    CSV_FILE = "finanice_data.csv"
+
+    @classmethod
+    def initialize_csv():
+        pass
