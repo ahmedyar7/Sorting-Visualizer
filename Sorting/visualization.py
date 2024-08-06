412fb13fcd95008f411002ece5267e8fb05fef78
ahmedyar7 <ahmedyar778@gmail.com>
2024-08-06 23:13:43 +0500

    Reseting the visulization class

--- Sorting/visualization.py
+++ Sorting/visualization.py
@@ -4,73 +4,62 @@ from draw_info import DrawInfo
 pygame.init()
 
 
-class Visualization:
+def draw(draw_info, algorithm_name, ascending) -> None:
 
-    def __init__(self, draw_info: DrawInfo) -> None:
-        self.draw_info = draw_info
+    algorithm_title = draw_info.LARGE_FONT.render(
+        f"{algorithm_name} - {"Ascending" if ascending else "Descending"}",
+        1,
+        draw_info.GREEN,
+    )
+    draw_info.window.blit(
+        algorithm_title,
+        (draw_info.width / 2 - algorithm_title.get_width() / 2, 65),
+    )
 
-    def draw(self, algorithm_name, ascending) -> None:
+    sorting = draw_info.SMALL_FONT.render(
+        "S - Selection Sort | X - Radix Sort | M - Merge Sort | H - Heap Sort | C - Counting Sort | B - Bubble Sort | I - Insertion Sort",
+        1,
+        draw_info.WHITE,
+    )
+    draw_info.window.blit(
+        sorting,
+        (draw_info.width / 2 - sorting.get_width() / 2, 35),
+    )
 
-        algorithm_title = self.draw_info.LARGE_FONT.render(
-            f"{algorithm_name} - {"Ascending" if ascending else "Descending"}",
-            1,
-            self.draw_info.GREEN,
+    controls = draw_info.SMALL_FONT.render(
+        "R - Reset | A - Ascending | D - Descending", draw_info.WHITE, 1
+    )
+    draw_info.window.blit(controls, (draw_info.width / 2 - controls.get_width() / 2, 5))
+
+
+def draw_list(draw_info, color_positions={}, clear_bg=False) -> None:
+
+    lst = draw_info.list
+
+    if clear_bg:
+        clear_rect = (
+            draw_info.SIDE_PADDING / 2,
+            draw_info.TOP_PADDING,
+            draw_info.width - draw_info.SIDE_PADDING,
+            draw_info.height - draw_info.TOP_PADDING,
         )
-        self.draw_info.window.blit(
-            algorithm_title,
-            (self.draw_info.width / 2 - algorithm_title.get_width() / 2, 65),
+
+        pygame.rect(clear_rect, draw_info.BACKGROUND_COLOR, draw_info.window)
+
+    for i, value in enumerate(lst):
+        X = draw_info.start_x + i * draw_info.block_width
+        Y = draw_info.start_x - (value - draw_info.min_value) * draw_info.block_width
+
+        colors = draw_info.GRADIENT[i % len(draw_info.GRADIENT)]
+
+        if i in color_positions:
+            colors = color_positions[i]
+
+        pygame.rect(
+            (X, Y, draw_info.block_width, draw_info.block_height),
+            colors,
+            draw_info.window,
         )
 
-        sorting = self.draw_info.SMALL_FONT.render(
-            "S - Selection Sort | X - Radix Sort | M - Merge Sort | H - Heap Sort | C - Counting Sort | B - Bubble Sort | I - Insertion Sort",
-            1,
-            self.draw_info.WHITE,
-        )
-        self.draw_info.window.blit(
-            sorting,
-            (self.draw_info.width / 2 - sorting.get_width() / 2, 35),
-        )
-
-        controls = self.draw_info.SMALL_FONT.render(
-            "R - Reset | A - Ascending | D - Descending", self.draw_info.WHITE, 1
-        )
-        self.draw_info.window.blit(
-            controls, (self.draw_info.width / 2 - controls.get_width() / 2, 5)
-        )
-
-    def draw_list(self, color_positions={}, clear_bg=False) -> None:
-
-        lst = self.draw_info.list
-
-        if clear_bg:
-            clear_rect = (
-                self.draw_info.SIDE_PADDING / 2,
-                self.draw_info.TOP_PADDING,
-                self.draw_info.width - self.draw_info.SIDE_PADDING,
-                self.draw_info.height - self.draw_info.TOP_PADDING,
-            )
-
-            pygame.rect(
-                clear_rect, self.draw_info.BACKGROUND_COLOR, self.draw_info.window
-            )
-
-        for i, value in enumerate(lst):
-            X = self.draw_info.start_x + i * self.draw_info.block_width
-            Y = (
-                self.draw_info.start_x
-                - (value - self.draw_info.min_value) * self.draw_info.block_width
-            )
-
-            colors = self.draw_info.GRADIENT[i % len(self.draw_info.GRADIENT)]
-
-            if i in color_positions:
-                colors = color_positions[i]
-
-            pygame.rect(
-                (X, Y, self.draw_info.block_width, self.draw_info.block_height),
-                colors,
-                self.draw_info.window,
-            )
-
-        if clear_bg:
-            pygame.display.update()
+    if clear_bg:
+        pygame.display.update()
